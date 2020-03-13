af0d9bd7d81aae14f62006b4146e6a383c7dc104
Aletar-r <61909596+Aletar-r@users.noreply.github.com>
2020-03-14 01:12:45 +0300

    1

---
+++ HW_2_1/HW_2_1/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+		int H;
+		int a;
+		std::cin>>H;
+	for (int i=0; i<H; i++) {
+		for (int t=0; t<(i+1); t++) {
+			std::cout << 0;
+		}
+		std::cout << std::endl;
+	}
+		std::cin>>a;
+
+}
+
